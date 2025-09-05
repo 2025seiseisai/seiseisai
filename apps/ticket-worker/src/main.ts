7913e2086b5f2ed240eef80e019b67be526e44f7
Rac <rac75116@gmail.com>
2025-09-06 01:38:10 +0900

    抽選を追加

--- apps/ticket-worker/src/main.ts
+++ apps/ticket-worker/src/main.ts
@@ -16,6 +16,38 @@ cron.schedule("30 * * * * *", async () => {
             },
         });
 
+        await dbClient.$transaction(async (tx) => {
+            // 当選したかつ引き換え締め切りを過ぎたがまだ整理券が引き換えられていない場合、ユーザーをBANする
+            await tx.ticketUser.updateMany({
+                where: {
+                    tickets: {
+                        some: {
+                            status: TicketStatus.当選,
+                            event: {
+                                eventStart: {
+                                    lt: now.toDate(),
+                                },
+                            },
+                        },
+                    },
+                    banned: false,
+                },
+                data: {
+                    banned: true,
+                },
+            });
+            // 引き換え期限を過ぎた整理券を削除する
+            await tx.ticket.deleteMany({
+                where: {
+                    event: {
+                        eventStart: {
+                            lt: now.toDate(),
+                        },
+                    },
+                },
+            });
+        });
+
         // 抽選がまだ行われていないイベントで、募集締め切りを過ぎているものを抽選
         const events = await dbClient.eventTicketInfo.findMany({
             where: {
@@ -27,25 +59,71 @@ cron.schedule("30 * * * * *", async () => {
         });
 
         for (const event of events) {
-            const tickets = await dbClient.ticket.findMany({
-                where: {
-                    eventId: event.id,
-                },
-            });
-            await dbClient.ticket.updateMany({
-                where: {
-                    id: { in: tickets.map((t) => t.id) },
-                },
-                data: {
-                    status: TicketStatus.落選, // TODO: ちゃんと実装する
-                },
-            });
-            await dbClient.eventDrawResult.create({
-                data: {
-                    eventId: event.id,
-                    totalApplications: tickets.length,
-                    winners: 0,
-                },
+            await dbClient.$transaction(async (tx) => {
+                const tickets = await tx.ticket.findMany({
+                    where: {
+                        eventId: event.id,
+                    },
+                });
+                const users = await tx.ticketUser.findMany({
+                    where: {
+                        tickets: {
+                            some: {
+                                eventId: event.id,
+                            },
+                        },
+                    },
+                });
+                const usersDict = users.reduce(
+                    (acc, user) => {
+                        acc[user.id] = user;
+                        return acc;
+                    },
+                    {} as Record<string, (typeof users)[0]>,
+                );
+                const capacity = event.capacity;
+                const winners: string[] = [];
+                let totalWinners = 0;
+                let totalApplications = 0;
+                for (let i = 0; i < tickets.length; i++) {
+                    const j = Math.floor(Math.random() * (tickets.length - i)) + i;
+                    [tickets[i], tickets[j]] = [tickets[j], tickets[i]];
+                    const ticket = tickets[i];
+                    totalApplications += ticket.paperTickets;
+                    const user = usersDict[ticket.userId];
+                    if (!user.banned && totalWinners + ticket.paperTickets <= capacity) {
+                        winners.push(ticket.id);
+                        totalWinners += ticket.paperTickets;
+                    }
+                }
+                await tx.ticket.updateMany({
+                    where: {
+                        id: {
+                            in: winners,
+                        },
+                    },
+                    data: {
+                        status: TicketStatus.当選,
+                    },
+                });
+                await tx.ticket.updateMany({
+                    where: {
+                        id: {
+                            notIn: winners,
+                        },
+                        eventId: event.id,
+                    },
+                    data: {
+                        status: TicketStatus.落選,
+                    },
+                });
+                await tx.eventDrawResult.create({
+                    data: {
+                        eventId: event.id,
+                        totalApplications: totalApplications,
+                        winners: totalWinners,
+                    },
+                });
             });
         }
     } catch (e) {
